3664abe34f4f758bea44d2c9463a6d21454ac7da
Lukas Hirsch <lukas.stag@gmail.com>
2026-01-28 18:02:15 +0100

    feat: add training log files and update plotting function to visualize multiple datasets

--- utils/graphics.py
+++ utils/graphics.py
@@ -1,36 +1,40 @@
 # Plot training progress
 import matplotlib.pyplot as plt
 import csv
-import numpy as np
 
 
-csv_path = "./training_log_tfdecay.csv"
+paths = [
+    "./training_log_tfdecay.csv",
+    "./training_log_sampled_tf.csv",
+]
 
-with open(csv_path, mode="r") as f:
-    reader = csv.DictReader(f, fieldnames=["timestamp", "step", "epoch", "loss", "lr"])
-    rows = [row for row in reader]
-    rows.pop(0)  # Remove header row if present
+def plot_training_progress(csv_path):
 
-#nthrows = rows[::50]
+    with open(csv_path, mode="r") as f:
+        reader = csv.DictReader(
+            f, fieldnames=["timestamp", "step", "epoch", "loss", "lr"]
+        )
+        rows = [row for row in reader]
+        rows.pop(0)  # Remove header row if present
+    plt.plot(
+        [int(row["step"]) for row in rows],
+        [float(row["loss"]) for row in rows],
+        marker="o",
+        linewidth=2,
+        markersize=6,
+        label=csv_path,
+    )
 
-#arr = np.array([float(row["loss"]) for row in rows])
-#ntlosses = np.mean(arr.reshape(-1, 1000), axis=1)
-#ntlosses = rows
 
 plt.figure(figsize=(10, 6))
-plt.plot(range(len(rows)), [float(row["loss"]) for row in rows], marker="o", linewidth=2, markersize=6)
+
+for csv_path in paths:
+    plot_training_progress(csv_path)
+
 plt.xlabel("Step", fontsize=12)
 plt.ylabel("Training Loss", fontsize=12)
 plt.title("Training Loss Over Time", fontsize=14, fontweight="bold")
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
+plt.legend()
 plt.show()
-
-print("Training Summary:")
-print(f"Initial Loss: {float(rows[0]['loss']):.4f}")
-print(f"Final Loss: {float(rows[-1]['loss']):.4f}")
-print(f"Steps: {int(rows[-1]['step']):,}")
-print(f"Best Loss: {min(float(row['loss']) for row in rows):.4f}")
-print(
-    f"Improvement: {((float(rows[0]['loss']) - float(rows[-1]['loss'])) / float(rows[0]['loss']) * 100):.1f}%"
-)
